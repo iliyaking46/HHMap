235f2d963ae766fb98e852b6d8bd6eba33895df8
Lara Opaleva <larachemik@yandex.ru>
2018-03-14 21:42:27 +0300

    Reimplemended store, refactored code

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import pure from 'recompose/pure'
 
-export const Button = ({onClick, children}) => (
+export const Button = ({ onClick, children }) => (
   <button className="btn btn-primary mx-3" onClick={onClick}>
     {children}
   </button>
-);
+)
 
-export default pure(Button);
+export default pure(Button)
---
+++ src/store/store.dev.js
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware } from 'redux'
+import ReduxThunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from 'reducer'
+
+export default function configureStore(initialState = {}) {
+  const middlewares = [ReduxThunk]
+  const enhancers = [
+    applyMiddleware(...middlewares),
+    // применяет мидлвеары
+  ]
+  const composeEnhancers = composeWithDevTools({
+    // other compose enhancers if any
+    // Specify here other options if needed
+  })
+  // createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk)))
+  const store = createStore(rootReducer, initialState, composeEnhancers(...enhancers))
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducer', () => {
+      /* eslint-disable global-require */
+      const nextReducer = require('../reducer').default
+      store.replaceReducer(nextReducer)
+    })
+  }
+
+  return store
+}
